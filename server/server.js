341d83a96b833fad25f6029ba8db05043aadc68d
John Fuhrmeister <61069716+JohnFuhrm12@users.noreply.github.com>
2022-05-02 21:31:10 -0400

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+const cors = require('cors');
+
+const PORT = process.env.PORT || 8000;
+const router = require('./router');
+
+io.on("connection", socket => {
+    socket.emit("your id", socket.id);
+    socket.on("send message", body => {
+        io.emit("message", body)
+    })
+})
+
+app.use(router);
+app.use(cors());
+
+server.listen(PORT, () => console.log("server is running on port 8000"));
