4025946d321adff2408b8ebbd5e81ce02453a757
John Fuhrmeister <61069716+JohnFuhrm12@users.noreply.github.com>
2022-05-06 01:44:36 -0400

    Add files via upload

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -4,13 +4,36 @@ import { Camera, CameraResultType } from '@capacitor/camera';
 import useLocalStorage from "./useLocalStorage";
 import './Chat.css';
 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { doc } from "firebase/firestore";
+import { UserSwitchOutlined } from "@ant-design/icons";
+import { getDocs } from "firebase/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCjmGw6wsKZ4iPtnyg_jpOIICWl_sFzwhk",
+  authDomain: "react-chat-app-85a86.firebaseapp.com",
+  projectId: "react-chat-app-85a86",
+  storageBucket: "react-chat-app-85a86.appspot.com",
+  messagingSenderId: "690348767137",
+  appId: "1:690348767137:web:bbbc7ee75a5cf058193de0"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 let socket;
 
 const Chat = () => {
   const [yourID, setYourID] = useState();
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
-  const [name, setName] = useLocalStorage()
+  const [name, setName] = useLocalStorage();
+  const [dbmessages, setDbmessages] = useState([]);
+  const messagesRef = firestore.collection('messages');
+  const scrollpoint = useRef();
   const ENDPOINT = 'https://react-chat-jf12.herokuapp.com/'
 
   const socketRef = useRef();
@@ -19,11 +42,19 @@ const Chat = () => {
     socket = io(ENDPOINT, { transports: ['websocket', 'polling', 'flashsocket'] });
     console.log(socket);
 
+    const getDbmessages = async () => {
+      const data = await getDocs(messagesRef.orderBy('createdAt', "desc").limit(5));
+      setDbmessages(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+      console.log(data);
+    };
+
+    getDbmessages();
+
     socketRef.current = io.connect('/');
 
     socket.on("your id", id => {
       setYourID(id);
-    })
+    }, [])
 
     socket.on("message", (message) => {
       receivedMessage(message);
@@ -34,14 +65,23 @@ const Chat = () => {
     setMessages(oldMsgs => [...oldMsgs, message]);
   }
 
-  function sendMessage(e) {
+  const sendMessage = async (e) => {
     e.preventDefault();
     const messageObject = {
       body: message,
       id: yourID,
     };
+
     socket.emit("send message", messageObject);
+
+    await messagesRef.add({
+      text: message,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      yourID,
+    })
+
     setMessage('');
+    scrollpoint.current.scrollIntoView({ behavior: 'smooth' })
   }
 
   function sendImage(e) {
@@ -63,44 +103,53 @@ const Chat = () => {
   }
 
   return (
-    <div class="page">
-      <div class="header">
+    <div className="page">
+      <div className="header">
         <h1>React Chat</h1>
-        <button onClick={handleLogout} class="logout">Logout</button>
+        <button onClick={handleLogout} className="logout">Logout</button>
       </div>
-      <div class="container">
+      <div className="container">
+        {dbmessages.slice().reverse().map((dbmessage) => {
+          return (
+          <div className="PartnerRow">
+            <div className="PartnerMessage">
+              {dbmessage.text}
+            </div>
+          </div>
+          );
+        })}
         {messages.map((message, index) => {
           if (message.id === yourID) {
             return (
               <div>
-                <div class="MyRow" key={index}> 
-                  <div class="MyMessage">
+                <div className="MyRow" key={index}> 
+                  <div className="MyMessage">
                     {message.body}
                   </div>
                 </div>
-                <div class="my-name">
+                <div className="my-name">
                   {name}
                 </div>
+                <div className="scrol" ref={scrollpoint}></div>
               </div>
-
             )
           }
           return (
-            <div class="PartnerRow" key={index}>
-              <div class="PartnerMessage">
+            <div className="PartnerRow" key={index}>
+              <div className="PartnerMessage">
                 {message.body}
               </div>
             </div>
           )
         })}
       </div>
-      <div class="footer">
-        <form class="img-form" onSubmit={sendImage}>
-          <button class="img-button"><img class="clip" src={require("./clip.png")} alt="paperclip" /></button>
+      <div className="footer">
+        <form className="img-form" onSubmit={sendImage}>
+          <button className="img-button"><img className="clip" src={require("./clip.png")} alt="paperclip" /></button>
         </form>
-        <form class="form" onSubmit={sendMessage}>
-          <textarea class="textarea" value={message} onChange={handleChange} placeholder="Say something..." required />
-          <button class="button">Send</button>
+        <form className="form" onSubmit={sendMessage}>
+          <textarea className="textarea" value={message} onChange={handleChange} placeholder="Say something..." required />
+          <button className="button">Send</button>
         </form>
       </div>
     </div>
